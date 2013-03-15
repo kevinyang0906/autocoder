3d25f3dc5e575dfad4ea51800f7f066df7e5aa34
hjyang <yhj@xueqiu.com>
2013-03-15 17:48:47 +0800

    edit dao

--- src/test/java/com/baidu/autocoder/AutoCoderTest.java
+++ src/test/java/com/baidu/autocoder/AutoCoderTest.java
@@ -9,8 +9,8 @@ public class AutoCoderTest {
 	@Test
 	public void testMain() {
 		/** 表名，对象名，描述，包名 **/
-		AutoCoder.main(new String[]{"usergroup","UserGroup","用户组","user"});
-		AutoCoder.main(new String[]{"member","Member","用户组成员","user"});
+		AutoCoder.main(new String[]{"prize","Prize","邀请兑换","invite"});
+//		AutoCoder.main(new String[]{"member","Member","用户组成员","user"});
 	}
 
 }
